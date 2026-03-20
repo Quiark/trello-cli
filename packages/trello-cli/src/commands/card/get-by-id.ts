cd9a82b30f728da3eafc1e51656ca5bc9a8ca5da
Roman Plášil <me@rplasil.name>
2026-03-20 10:33:02 +0800

    Add option to include comments with card

--- packages/trello-cli/src/commands/card/get-by-id.ts
+++ packages/trello-cli/src/commands/card/get-by-id.ts
@@ -8,17 +8,31 @@ export default class GetById extends BaseCommand<typeof GetById> {
 
   static flags = {
     id: Flags.string({ required: true, description: "The Trello card ID" }),
+    comments: Flags.boolean({
+      description: "Include comments on the card",
+      default: false,
+    }),
   };
 
   async run(): Promise<void> {
     const card = await this.client.cards.getCard({
       id: this.flags.id,
     });
-    this.output(card);
+
+    let comments: any[] = [];
+    if (this.flags.comments) {
+      const actions = await this.client.cards.getCardActions({
+        id: this.flags.id,
+        filter: "commentCard",
+      });
+      comments = actions;
+    }
+
+    this.output({ ...card, comments });
   }
 
   protected async toData(data: any) {
-    return {
+    const result: any = {
       id: data.id,
       name: data.name,
       due: data.due,
@@ -28,6 +42,17 @@ export default class GetById extends BaseCommand<typeof GetById> {
       members: await this.cache.convertMemberIdsToEntity(data.idMembers),
       list: data.idList,
     };
+
+    if (data.comments && data.comments.length > 0) {
+      result.comments = data.comments.map((action: any) => ({
+        id: action.id,
+        text: action.data.text,
+        author: action.memberCreator.fullName,
+        date: action.date,
+      }));
+    }
+
+    return result;
   }
 
   protected async format(data: any): Promise<string> {
@@ -44,6 +69,15 @@ export default class GetById extends BaseCommand<typeof GetById> {
       `List ID: ${data.list}`,
       `Description: ${data.description || "None"}`,
     ];
+
+    if (data.comments && data.comments.length > 0) {
+      lines.push("");
+      lines.push("Comments:");
+      data.comments.forEach((c: any) => {
+        lines.push(`  ${c.author} [${c.date}]: ${c.text}`);
+      });
+    }
+
     return lines.join("\n");
   }
 }
